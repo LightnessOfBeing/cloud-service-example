fb4d2aa51b0af29bc717078ce9f0e939b564b377
Kirill <intainer7@gmail.com>
2021-05-01 14:24:10 +0300

    removed lines

--- docker/src/api.py
+++ docker/src/api.py
@@ -5,6 +5,4 @@ def upload_file(file_name, bucket):
     object_name = file_name
     s3_client = boto3.client('s3')
     response = s3_client.upload_file(file_name, bucket, object_name)
-    s3 = boto3.resource('s3')
-    s3.Bucket(bucket).download_file(file_name, f"./downloads/{file_name}")
     return response
