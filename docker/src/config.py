bce1231213ea5321e394e2f6f679ecddf286608c
Kirill <intainer7@gmail.com>
2021-05-01 15:22:08 +0300

    fix

--- docker/src/config.py
+++ docker/src/config.py
@@ -9,4 +9,4 @@ AWS_SECRET_ACCESS_KEY = credentials.secret_key
 
 S3_BUCKET               = "my-app-bucket-kirill"
 S3_LOCATION             = f'http://{S3_BUCKET}.s3.amazonaws.com/'
-print(AWS_ACESS_KEY_ID, AWS_SECRET_ACCESS_KEY)
+print(AWS_ACCESS_KEY_ID, AWS_SECRET_ACCESS_KEY)
