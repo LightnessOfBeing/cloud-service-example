281177cc3250823ab326d6c38cf56cafa0ea82af
Kirill <intainer7@gmail.com>
2021-05-01 15:17:26 +0300

    fix

--- docker/src/config.py
+++ docker/src/config.py
@@ -1,10 +1,11 @@
-import os
+import boto3
+
+session = boto3.Session()
+credentials = session.get_credentials()
+
+credentials = credentials.get_frozen_credentials()
+AWS_ACCESS_KEY_ID = credentials.access_key
+AWS_SECRET_ACCESS_KEY = credentials.secret_key
 
 S3_BUCKET               = "my-app-bucket-kirill"
-AWS_ACCESS_KEY_ID       = os.environ.get("AWS_ACCESS_KEY_ID")
-AWS_SECRET_ACCESS_KEY   = os.environ.get("AWS_SECRET_ACCESS_KEY")
 S3_LOCATION             = f'http://{S3_BUCKET}.s3.amazonaws.com/'
-
-SECRET_KEY                = os.urandom(32)
-DEBUG                     = True
-PORT                      = 5000
