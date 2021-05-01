157665016d1414eced4ed9ddcc2de2b59ab01a76
Kirill <intainer7@gmail.com>
2021-05-01 16:20:08 +0300

    test message added

--- docker/app.py
+++ docker/app.py
@@ -13,7 +13,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def entry_point():
-    return f'Hello World! Test Message! '
+    return f'Hello World! Test Message! Yet another test message!'
 
 
 @app.route("/storage")
